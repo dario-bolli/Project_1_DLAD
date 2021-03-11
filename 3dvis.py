bf35fead864ee69e6a7a88f81a785578df40278e
nhoischen <nhoischen@student.ethz.ch>
2021-03-11 23:13:14 +0100

    update 3dvis.py with color point cloud

--- 3dvis.py
+++ 3dvis.py
@@ -31,14 +31,27 @@ class Visualizer():
                                    [5,1],[5,4],[5,6],
                                    [7,3],[7,4],[7,6]])
 
-    def update(self, points):
+    def update(self, points, sem_label, color_map):
         '''
         :param points: point cloud data
                         shape (N, 3)          
         Task 2: Change this function such that each point
         is colored depending on its semantic label
         '''
-        self.sem_vis.set_data(points, size=3)
+        #Normalize colors, (and reverse BGR->RGB?)
+        
+        for i in color_map:
+            color_map[i] = np.array(color_map[i])/255
+
+        # declare color as numpy array, N x 3
+        color = np.zeros((points.shape[0],3))
+
+        # get color for each point
+        for i in range(points.shape[0]):
+            label=sem_label[i][0].astype(np.uint8)
+            color[i,:] = color_map.get(label)
+        # Plot point cloud with colors
+        self.sem_vis.set_data(points, size=3, face_color = color)
     
     def update_boxes(self, corners):
         '''
@@ -64,79 +77,12 @@ class Visualizer():
                               width=2,
                               color=[0,1,0,1])
 
-def z_rotation(theta):
-    """
-    Rotation about the z-axis. (y in cam0 coordinates)
-    """
-    c = np.cos(theta)
-    s = np.sin(theta)
-    """
-    Rot= np.array([[c, 0, s],
-                     [0, 1, 0],
-                     [-s, 0, c]])
-    Rot = coordCam0toVelo(Rot)
-    """              
-    Rot = np.array([[c, s, 0],
-                     [-s, c, 0],
-                     [0, 0, 1]])
-                     
-    return Rot
-
-def coordCam0toVelo(points):
-    """
-    :param vector of points (x,y,z) in camera 0 coordinates
-    return: vector of points (x,y,z) in the velodyne frame
-    """
-    points.shape = (3,-1) # vector form
-    # Inverse transformation in homogenous coordinates
-    T_inv = np.linalg.inv(data['T_cam0_velo'])
-    homogenous_points = np.vstack((points, np.ones((1,points.shape[1]))))
-    pointsLidar = T_inv @ homogenous_points
-    return pointsLidar[:3,:]
-    
-def box_corner_coordinates(data):
-      
-    for i in range(len(data)):  
-        # extract dimensions of the box
-        height = data[i][8]
-        width = data[i][9]
-        length = data[i][10]
-        
-        # extract location in camera0 coordinates and project into Velodyne frame
-        CenterBox_lidar = coordCam0toVelo(np.array((data[i][11], \
-                                          data[i][12],data[i][13])))
-
-        # Corners location in 3D in velodyne coordinates
-        x = [length/2, length/2, -length/2, -length/2, length/2, length/2, -length/2, -length/2]
-        y = [-width/2, width/2, width/2, -width/2,-width/2, width/2, width/2, -width/2]
-        z = [height,height, height, height, 0, 0, 0, 0 ]
-        # Dimensions of box_dim: 3 x 8 i.e. rows (x,y,z) and columns are the corners
-        box_dim = np.vstack([x,y,z])
-
-        #rotation of axe 
-        Rot = z_rotation(data[i][14])
-        #box_dim = Rot @ box_dim #rotation GIVES WRONG OUTPUT WHY?
-
-        # Center the box around the object location in the Velodyne frame
-        box_dim += CenterBox_lidar
-        box_dim = box_dim.T # coordinates as columns and corner numbers as rows
-      
-        if i == 0: # first object
-            corners = box_dim 
-        else: # stack up object boxes coordinates in one array
-            corners = np.dstack((box_dim, corners)) # Shape is 8 x 3 x N
-    
-    return np.transpose(corners, (2,0,1)) # correct format for drawing N x 8 x 3
-
-
 if __name__ == '__main__':
     
     data = load_data('data/demo.p') # Change to data.p for your final submission 
     visualizer = Visualizer() 
-    # Get corner coordinates in Velodyne frame
-    corners = box_corner_coordinates(data['objects']) 
-    visualizer.update_boxes(corners[:,:,:]) # Draw the boxes in 3D
-    visualizer.update(data['velodyne'][:,:3]) # Point cloud
+  
+    visualizer.update(data['velodyne'][:,:3],data['sem_label'], data['color_map']) # Point cloud
     '''
     Task 2: Compute all bounding box corners from given
     annotations. You can visualize the bounding boxes using
